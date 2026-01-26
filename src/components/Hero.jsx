4eef0e172c254b1278a096e6a7a514a3512080e1
quentin <quentin@epitech.eu>
2026-01-26 18:43:51 +0100

    update version three.js

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,6 +2,7 @@
 import { motion } from 'motion/react'
 import { styles } from '../styles'
 import { resume } from '../assets'
+import { ComputersCanvas } from './canvas';
 
 
 const Hero = () => {
@@ -41,7 +42,7 @@ const Hero = () => {
         </div>
     </div>
 
-   {/* <ComputersCanvas /> */}
+        {/* <ComputersCanvas /> */}
 
       <div className=' absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
         <a href='#about'>
